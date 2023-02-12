4bbf6fd6bde3fb82f15bb6e90e71149d3b7a37d5
JackieHanLab <79742883+JackieHanLab@users.noreply.github.com>
2023-02-12 14:00:07 +0800

    Update pre.py

--- TOSICA/pre.py
+++ TOSICA/pre.py
@@ -5,9 +5,8 @@ import pandas as pd
 import numpy as np
 import torch.nn.functional as F
 import scanpy as sc
+import anndata as ad
 from .TOSICA_model import scTrans_model as create_model
-
-
 
 #model_weight_path = "./weights20220429/model-5.pth" 
 mask_path = os.getcwd()+'/mask.npy'
@@ -50,6 +49,11 @@ def prediect(adata,model_weight_path,mask_path = mask_path,laten=False,save_att 
     pathway = pd.read_csv(os.getcwd()+'/pathway.csv', index_col=0)
     dictionary = pd.read_table(os.getcwd()+'/label_dictionary.csv', sep=',',header=0,index_col=0)
     n_c = len(dictionary)
+    label_name = dictionary.columns[0]
+    dictionary.loc[(dictionary.shape[0])] = 'Unknown'
+    dic = {}
+    for i in range(len(dictionary)):
+        dic[i] = dictionary[label_name][i]
     model = create_model(num_classes=n_c, num_genes=num_genes,mask = mask, has_logits=False,depth=depth,num_heads=num_heads).to(device)
     # load model weights
     model.load_state_dict(torch.load(model_weight_path, map_location=device))
@@ -73,12 +77,7 @@ def prediect(adata,model_weight_path,mask_path = mask_path,laten=False,save_att 
     latent = torch.squeeze(latent).cpu().numpy()
     l_p = np.c_[latent, predict_class,pre_class]
     att = np.c_[att, predict_class,pre_class]
-    all_l_p = pd.DataFrame(l_p)
-    all_att = pd.DataFrame(att)
-    all_l_p.rename(columns={embed_dim:'Prediction',embed_dim+1:'Probability'},inplace = True)
-    all_att.rename(columns={len(pathway):'Prediction',len(pathway)+1:'Probability'},inplace = True)
     all_line = adata.shape[0]
-    #all_line = int(all_line.split()[0])
     n_line = 0
     while (n_line) <= all_line:
         if (all_line-n_line)%batch_size != 1:
@@ -97,13 +96,11 @@ def prediect(adata,model_weight_path,mask_path = mask_path,laten=False,save_att 
                                                  pin_memory=True)
         with torch.no_grad():
             # predict class
+            adata_list = []
             for step, data in enumerate(data_loader):
                 #print(step)
                 exp = data
                 lat, pre, weights = model(exp.to(device))
-                #weights = get_weight(weights,pathway)
-                #att = torch.cat((att,weights),0)
-                #latent = torch.cat((latent,lat),0)
                 pre = torch.squeeze(pre).cpu()
                 pre = F.softmax(pre,1)
                 predict_class = np.empty(shape=0)
@@ -114,39 +111,24 @@ def prediect(adata,model_weight_path,mask_path = mask_path,laten=False,save_att 
                     else:
                         predict_class = np.r_[predict_class,n_c]
                     pre_class = np.r_[pre_class,torch.max(pre, dim=1)[0][i]]     
-                latent = torch.squeeze(lat).cpu().numpy()
-                weights = torch.squeeze(weights).cpu().numpy()
-                l_p = np.c_[latent, predict_class,pre_class]
-                att = np.c_[weights, predict_class,pre_class]
-                l_p = pd.DataFrame(l_p)
-                att = pd.DataFrame(att)
-                l_p.rename(columns={embed_dim:'Prediction',embed_dim+1:'Probability'},inplace = True)
-                att.rename(columns={(len(pathway)):'Prediction',(len(pathway)+1):'Probability'},inplace = True)
-                all_att = pd.concat([all_att,att],ignore_index = True)
-                all_l_p = pd.concat([all_l_p,l_p],ignore_index = True)
-                #l_p.to_csv(dataset+'_pre_'+str(cutoff)+'.csv',mode='a', header=0,index=False) 
-                #att.to_csv(dataset+'_att_'+str(cutoff)+'.csv',mode='a', header=0,index=False)
+                l_p = torch.squeeze(lat).cpu().numpy()
+                att = torch.squeeze(weights).cpu().numpy()
+                meta = np.c_[predict_class,pre_class]
+                meta = pd.DataFrame(meta)
+                meta.columns = ['Prediction','Probability']
+                meta.index = meta.index.astype('str')
+                if laten:
+                    l_p = l_p.astype('float32')
+                    new = sc.AnnData(l_p, obs=meta)
+                else:
+                    att = att[:,0:(len(pathway)-n_unannotated)]
+                    att = att.astype('float32')
+                    varinfo = pd.DataFrame(pathway.iloc[0:(len(pathway)-1),0].values,index=pathway.iloc[0:(len(pathway)-1),0],columns=['pathway_index'])
+                    new = sc.AnnData(att, obs=meta, var = varinfo)
+                adata_list.append(new)
     print(all_line)
-    dictionary = pd.read_table(os.getcwd()+'/label_dictionary.csv', sep=',',header=0,index_col=0) 
-    label_name = dictionary.columns[0]
-    dictionary.loc[(dictionary.shape[0])] = 'Unknown'
-    dic = {}
-    for i in range(len(dictionary)):
-        dic[i] = dictionary[label_name][i]
-    if laten:
-        all_l_p['Prediction'] = all_l_p['Prediction'].map(dic)
-        meta = pd.DataFrame(all_l_p[["Prediction",'Probability']])
-        all_l_p = all_l_p.iloc[:,0:embed_dim]
-        new = sc.AnnData(all_l_p, obs=meta)
-        new.obs[adata.obs.columns] = adata.obs[adata.obs.columns].values
-        return(new)
-    else:
-        all_att['Prediction'] = all_att['Prediction'].map(dic)
-        meta = pd.DataFrame(all_att[["Prediction",'Probability']])
-        meta.index = adata.obs.index
-        all_att = all_att.iloc[:,0:len(pathway)]
-        all_att.columns = pathway.iloc[:,0]
-        varinfo = pd.DataFrame(all_att.columns.values,index=all_att.columns,columns=['pathway_index'])
-        new = sc.AnnData(all_att, obs=meta, var = varinfo)
-        new.obs[adata.obs.columns] = adata.obs[adata.obs.columns].values
-        return(new)
+    new = ad.concat(adata_list)
+    new.obs.index = adata.obs.index
+    new.obs['Prediction'] = new.obs['Prediction'].map(dic)
+    new.obs[adata.obs.columns] = adata.obs[adata.obs.columns].values
+    return(new)
